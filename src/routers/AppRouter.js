9520ceb1d73a60196506772386ae293d1f230391
Neelanjan.Sen <neelanjan.sen@stackroute.in>
2018-09-17 23:56:37 +0530

    initial commit

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from '../components/Header.js';
-import ExpenseDashboardPage from '../components/ExpenseDashboardPage.js';
-import AddExpensePage from '../components/AddExpensePage.js';
-import EditExpensePage from '../components/EditExpensePage.js';
-import HelpPage from '../components/HelpPage.js';
+import Contact from '../components/Contact.js';
+import Home from '../components/Home.js';
+import Portfolio from '../components/Portfolio.js';
 import NotFoundPage from '../components/NotFoundPage.js';
+import PortfolioItem from '../components/PortfolioItem.js';
 
 const AppRouter = () => (
   <BrowserRouter>
     <div>
       <Header />
       <Switch>
-        <Route path="/" component={ExpenseDashboardPage} exact={true} />
-        <Route path="/create" component={AddExpensePage} />
-        <Route path="/edit/:id" component={EditExpensePage} />
-        <Route path="/help" component={HelpPage} />
+        <Route path="/" component={Home} exact={true} />
+        <Route path="/portfolio" component={Portfolio} exact={true}/>
+        <Route path="/portfolio/:id" component={PortfolioItem} />
+        <Route path="/contact" component={Contact} />
         <Route component={NotFoundPage} />
       </Switch>
     </div>  
